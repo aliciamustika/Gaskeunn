06deac434de24ae155a927c8cd16dc0e7bc87292
Alicia <aliciamustika07@gmail.com>
2025-12-27 18:07:02 +0700

    Add GitHub Actions deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/Gaskeunn',
+  base: '/Gaskeunn/', // ← UBAH INI sesuai nama repo
   plugins: [
     react(),
     tailwindcss()
